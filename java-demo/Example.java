f1b384f2b8d24ca5d314c47bb944026c004e1a42
vishwaka-kamlesh <vishwaka.kamlesh@gmail.com>
2025-11-14 14:43:30 +0530

    bad code

--- java-demo/Example.java
+++ java-demo/Example.java
@@ -1,5 +1,10 @@
 public class Example {
     public static void main(String[] args) {
+        String a = "hello";
+        String b = new String("hello");
+        if (a == b) {
+            System.out.println("Equal!");
+        }
 
     }
 }
