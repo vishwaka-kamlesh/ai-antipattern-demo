2c91c90d2875ed8aee8362c3424a0a1908874fc9
vishwaka-kamlesh <vishwaka.kamlesh@gmail.com>
2025-11-14 16:40:18 +0530

    bad try catch code

--- java-demo/Example.java
+++ java-demo/Example.java
@@ -1,5 +1,8 @@
 public class Example {
     public static void main(String[] args) {
-
+        try {
+            throw new RuntimeException("boom");
+        } catch (Exception e) {
+        }
     }
 }
