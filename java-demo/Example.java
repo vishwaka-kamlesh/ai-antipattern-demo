4f5041f4770d3ebf2a0dc25d22a8b9cb70c3ac8d
vishwaka-kamlesh <vishwaka.kamlesh@gmail.com>
2025-11-14 16:31:34 +0530

    demo

--- java-demo/Example.java
+++ java-demo/Example.java
@@ -5,34 +5,5 @@ public class Example {
         if (a == b) {
             System.out.println("Equal!");
         }
-
-        if ("a" == "a") {
-            System.out.println("Equal!");
-        }
-
-        
-        if ("a" == "a") {
-            System.out.println("Equal!");
-        }
-
-        if (1L == 1L) {
-            System.out.println("Equal!");
-        }
-
-        if (1L == 1L) {
-            System.out.println("Equal!");
-        }
-
-        String c = "hello";
-        String d = new String("hello");
-        if (c == d) {
-            System.out.println("Equal!");
-        }
-
-        String e = "hello";
-        String f = new String("hello");
-        if (e == f) {
-            System.out.println("Equal!");
-        }
     }
 }
