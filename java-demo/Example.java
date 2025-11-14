65e51f34a7984b8750989d21adb7f42b9d570536
vishwaka-kamlesh <vishwaka.kamlesh@gmail.com>
2025-11-14 13:09:43 +0530

    demo

--- java-demo/Example.java
+++ java-demo/Example.java
@@ -1,28 +1,5 @@
 public class Example {
     public static void main(String[] args) {
-        String a = "hello";
-        String b = new String("hello");
-        if (a == b) {
-            System.out.println("Equal!");
-        }
 
-        Integer m = 1;
-        Integer n = 1;
-        if (m == n) {
-            System.out.println("Equal!");
-        }
-
-        String s1 = "hello";
-        String s2 = new String("hello");
-        if (s1 == s2) {
-            System.out.println("Equal!");
-        }
-        
-
-        String s3 = "hello";
-        String s4 = new String("hello");
-        if (s3 == s4) {
-            System.out.println("Equal!");
-        }
     }
 }
