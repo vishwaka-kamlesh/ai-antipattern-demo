e1c09529a4ae9f002eceb5408ea887f36c7b9b5a
vishwaka-kamlesh <vishwaka.kamlesh@gmail.com>
2025-11-14 16:51:51 +0530

    demo

--- java-demo/Example.java
+++ java-demo/Example.java
@@ -1,8 +1,5 @@
 public class Example {
     public static void main(String[] args) {
-        try {
-            throw new RuntimeException("boom");
-        } catch (Exception e) {
-        }
+
     }
 }
